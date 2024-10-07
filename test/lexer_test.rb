bc81d6b773e828b9c04b7b7b2ad9fb38d8ae9b7c
Ali Hamdi Ali Fadel <aliosm1997@gmail.com>
2024-10-07 16:40:14 +0300

    Enhance unit tests structure

---
+++ test/lexer_test.rb
@@ -0,0 +1,115 @@
+# frozen_string_literal: true
+
+require 'minitest/autorun'
+
+require 'lexer'
+
+module Zaid
+  class LexerTest < Minitest::Test
+    include Keywords
+
+    def setup
+      @lexer = Lexer.new
+    end
+
+    def test_tokenize
+      assert_equal [[:IDENTIFIER, 'عدد'], ['=', '='], [:NUMBER, 5]], @lexer.tokenize('عدد = ٥')
+    end
+
+    def test_tokenize_without_compression
+      code = <<~CODE
+        إذا كان ٥ أكبر من ٣ إذن
+          اطبع("٥ أكبر من ٣")
+      CODE
+
+      tokens = [
+        [:IF, IF],
+        [:WAS, WAS],
+        [:NUMBER, 5],
+        [:GREATER, GREATER],
+        [:THAN, THAN],
+        [:NUMBER, 3],
+        [:THEN, THEN],
+        [:INDENT, 2],
+        [:IDENTIFIER, 'اطبع'],
+        ['(', '('],
+        [:STRING, '٥ أكبر من ٣'],
+        [')', ')'],
+        [:DEDENT, 0]
+      ]
+
+      assert_equal tokens, @lexer.tokenize(code, run_compression: false)
+    end
+
+    def test_tokenize_with_compression
+      code = <<~CODE
+        إذا كان ٥ أكبر من ٣ إذن
+          اطبع("٥ أكبر من ٣")
+      CODE
+
+      tokens = [
+        [:IF, "#{IF} #{WAS}"],
+        [:NUMBER, 5],
+        ['>', '>'],
+        [:NUMBER, 3],
+        [:THEN, THEN],
+        [:INDENT, 2],
+        [:IDENTIFIER, 'اطبع'],
+        ['(', '('],
+        [:STRING, '٥ أكبر من ٣'],
+        [')', ')'],
+        [:DEDENT, 0]
+      ]
+
+      assert_equal tokens, @lexer.tokenize(code, run_compression: true)
+    end
+
+    def test_compress
+      tokens = [
+        [:IF, IF],
+        [:WAS, WAS],
+        [:NUMBER, 5],
+        [:GREATER, GREATER],
+        [:THAN, THAN],
+        [:NUMBER, 3],
+        [:THEN, THEN],
+        [:INDENT, 2],
+        [:IDENTIFIER, 'اطبع'],
+        ['(', '('],
+        [:STRING, '٥ أكبر من ٣'],
+        [')', ')'],
+        [:DEDENT, 0],
+        [:ELSE, ELSE],
+        [:INDENT, 2],
+        [:IDENTIFIER, 'اطبع'],
+        ['(', '('],
+        [:STRING, '٥ أصغر من أو يساوي ٣'],
+        [')', ')'],
+        [:DEDENT, 0]
+      ]
+
+      compressed = [
+        [:IF, "#{IF} #{WAS}"],
+        [:NUMBER, 5],
+        ['>', '>'],
+        [:NUMBER, 3],
+        [:THEN, THEN],
+        [:INDENT, 2],
+        [:IDENTIFIER, 'اطبع'],
+        ['(', '('],
+        [:STRING, '٥ أكبر من ٣'],
+        [')', ')'],
+        [:DEDENT, 0],
+        [:ELSE, ELSE],
+        [:INDENT, 2],
+        [:IDENTIFIER, 'اطبع'],
+        ['(', '('],
+        [:STRING, '٥ أصغر من أو يساوي ٣'],
+        [')', ')'],
+        [:DEDENT, 0]
+      ]
+
+      assert_equal compressed, @lexer.compress(tokens)
+    end
+  end
+end
