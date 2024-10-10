d140d1b9d2495ab913b2c6e47b28824220ba699c
Ali Hamdi Ali Fadel <aliosm1997@gmail.com>
2024-10-10 15:29:16 +0300

    Add bin/console and zaid entry file

---
+++ lib/zaid.rb
@@ -0,0 +1,12 @@
+# frozen_string_literal: true
+
+require_relative 'zaid/version'
+require_relative 'zaid/keywords'
+require_relative 'zaid/lexer'
+require_relative 'zaid/nodes'
+require_relative 'zaid/parser'
+require_relative 'zaid/runtime'
+require_relative 'zaid/interpreter'
+
+module Zaid
+end
